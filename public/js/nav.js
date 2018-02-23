9764b3d8f8be4036f9e5f8f94620e03d9cf95c27
SantiagoIzzo <saanti.izzo@gmail.com>
2018-02-23 02:54:34 -0300

    corrigio nav

--- public/js/nav.js
+++ public/js/nav.js
@@ -1,17 +1,12 @@
 var nav=document.getElementById("nav")
-var header=document.getElementById("header")
-// var redes=document.getElementById("contenedor-redes-nav")
-var posicionesNav=nav.getBoundingClientRect();
-var posicionesHeader=header.getBoundingClientRect();
-const posicionInicial=posicionesHeader.bottom
+var sticky = nav.offsetTop;
 
-window.onscroll=function(){
-    if (document.body.scrollTop >= posicionesNav.bottom || document.documentElement.scrollTop >= posicionesNav.bottom){
-        nav.style.position= "fixed"
-        nav.style.top= "0"
-        // redes.style.display= "inline-block"
-    } else {   
-        nav.style.position= "inherit"
-        // redes.style.display= "none"
+
+window.onscroll = function() {
+    if (window.pageYOffset >= sticky) {
+        nav.classList.add("sticky");
+    }else{
+        nav.classList.remove("sticky");
     }
-}
+};
+
